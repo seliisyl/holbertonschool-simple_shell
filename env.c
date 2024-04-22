b9612f5ab553fcf62f51b2414bd6856303dc87a2
seliisyl <158849359+seliisyl@users.noreply.github.com>
2024-04-22 10:57:14 +0200

    Create env.c

---
+++ env.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Fonction pour afficher l'environnement */
+void show_environment() {
+    system("env");
+}
+
+/* Fonction pour définir une variable d'environnement */
+void set_environment_variable(char *name, char *value) {
+    if (name == NULL || value == NULL) {
+        fprintf(stderr, "Usage: setenv <variable> <valeur>\n");
+        return;
+    }
+    setenv(name, value, 1);
+}
+
+/* Fonction pour supprimer une variable d'environnement */
+void unset_environment_variable(char *name) {
+    if (name == NULL) {
+        fprintf(stderr, "Usage: unsetenv <variable>\n");
+        return;
+    }
+    unsetenv(name);
+}
