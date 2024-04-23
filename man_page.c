8e8ef90ed76bbd9e66c7b2c790c8b50704ca9a39
paulineonier <pauline.onier@outlook.fr>
2024-04-23 12:22:12 +0000

    correcting betty again

---
+++ man_page.c
@@ -0,0 +1,39 @@
+.\" Manpage for simple_shell
+.TH simple_shell  "April 2024" "User Commands"
+
+.SH NAME
+simple_shell \- Holberton School project shell
+
+.SH SYNOPSIS
+.B #include <main.h>
+.sp
+.B int main(__attribute__((unused)) int ac, char **av);
+
+.SH DESCRIPTION
+simple_shell is a basic Unix shell developed as a project for Holberton School. It provides a command-line interface for users to interact with the operating system.
+
+.SH COMMANDS
+       The shell supports various standard shell commands, including but not
+       limited to:
+
+       - cd: Change directory.
+       - ls: List directory contents.
+       - pwd: Print current working directory.
+       - echo: Display a message.
+       - exit: Exit the shell.
+
+.SH RETURN VALUE
+
+.SH EXAMPLES
+
+.SH DATE
+
+April 26, 2024
+
+.SH BUGS 
+	No know bugs
+
+.SH AUTHORS
+        SYLLA Selimata, 
+	ONIER Pauline, 
+	LAURIER Aline
